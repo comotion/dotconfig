aed399be1a258af27de666ac0af3c5a2ccf0931c
Olav Frengstad <olav@fwt.no>
2013-11-03 11:58:08 -0500

    Add dynamic workspaces w/cycling to xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,24 @@
 import XMonad
+import XMonad.Prompt
+import qualified XMonad.StackSet as W
+
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.EwmhDesktops
 
 import XMonad.Actions.PhysicalScreens
+import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.CopyWindow(copy, kill1)
+import XMonad.Actions.CycleWS
+
+
 
 import XMonad.Layout.NoBorders
 import XMonad.Layout.GridVariants
 
 import ScratchPadKeys (scratchPadList, manageScratchPads, scratchPadKeys)
 import XMonad.Util.EZConfig (additionalKeysP)
+
+---import Data.List ( findIndex )
 
 main = do
 	xmonad $ defaultConfig
@@ -21,29 +31,53 @@ main = do
 		, manageHook         = myManageHook
 		} `additionalKeysP` myKeys
 
+myWorkspaces = ["misc"]
+
 myManageHook :: ManageHook
 myManageHook = composeAll [] <+> manageScratchPads scratchPadList
 
-myWorkspaces = ["1:view", "2:code", "3:x", "4:y", "5:z", "6:w", "7:z", "8:media", "9:communicate"]
+
+---nextOrNewWorkspace = workspace (current ws)
+---	where ws = gets windowset
+---
+---findWsIndex :: WindowSpace -> [WindowSpace] -> Maybe Int
+---findWsIndex ws wss = findIndex ((== tag ws) . tag) wss
+
 
 myKeys :: [(String, X())]
 myKeys = [
 	  ("M-d"                        , spawn "dmenu_run"                         )
+
 	, ("<XF86AudioMute>"            , spawn "amixer sset Master toggle"     )
 	, ("<XF86AudioRaiseVolume>"     , spawn "amixer sset Master 1%+ unmute" )
 	, ("<XF86AudioLowerVolume>"     , spawn "amixer sset Master 1%- unmute" )
 	, ("<XF86AudioNext>"            , spawn "DISPLAY=:0 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"          )
 	, ("<XF86AudioPrev>"            , spawn "DISPLAY=:0 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"      )
 	, ("<XF86AudioPlay>"            , spawn "DISPLAY=:0 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"     )
-	, ("M-w"                        , viewScreen 0)
-	, ("M-e"                        , viewScreen 1)
-	, ("M-r"                        , viewScreen 2)
+
 	, ("M-="                        , sendMessage $ IncMasterCols 1)
 	, ("M--"                        , sendMessage $ IncMasterCols (-1))
 	, ("M-S-="                      , sendMessage $ IncMasterRows 1)
 	, ("M-S--"                      , sendMessage $ IncMasterRows (-1))
+
+	, ("M-<Backspace>"              , removeWorkspace)
+	, ("M-v"                        , selectWorkspace defaultXPConfig)
+	, ("M-m"                        , withWorkspace defaultXPConfig (windows . W.shift))
+	, ("M-c"                        , withWorkspace defaultXPConfig (windows . copy))
+	, ("M-S-r"                      , renameWorkspace defaultXPConfig)
+
+	, ("M-<Left>"                   , prevWS)
+	, ("M-<Right>"                  , nextWS)
+	, ("M-S-<Left>"                 , shiftToPrev >> prevWS)
+	, ("M-S-<Right>"                , shiftToNext >> nextWS)
+
+	, ("M-w"                        , viewScreen 0)
+	, ("M-e"                        , viewScreen 1)
+	, ("M-r"                        , viewScreen 2)
 	, ("M-S-w"                      , sendToScreen 0)
 	, ("M-S-e"                      , sendToScreen 1)
 	, ("M-S-r"                      , sendToScreen 2)
+
+	, ("M-S-c"                      , kill1)
 	, ("M-S-q"                      , spawn "xfce4-session-logout")
 	] ++ scratchPadKeys scratchPadList
